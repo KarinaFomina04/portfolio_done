96c68d76b49a89938a6cf0fd5763dc8262b0c3e4
KarinaFomina04 <karinafomina.19970804@gmail.com>
2023-06-14 23:44:12 +0200

    Add links: github and linkedin

--- src/components/Greeting/Greeting.tsx
+++ src/components/Greeting/Greeting.tsx
@@ -10,6 +10,18 @@ export const Greeting: React.FC = () => {
                 {/*<img src="" alt=""/>*/}
                 <p>Hi, I'm Karyna Kryshneva. A passionate Front-end React Developer based in Poznan, Poland.
                     📍</p>
+                <span>
+                    <a aria-label="linkedin" rel="noreferrer" target="_blank" href="https://www.linkedin.com/in/karina-kryshneva-192a4a221/">
+                        <i className="fa-brands fa-linkedin">
+                            <img src="/linkedin.svg" />
+                        </i>
+                    </a>
+                    <a aria-label="linkedin" rel="noreferrer" target="_blank" href="https://www.linkedin.com/in/karina-kryshneva-192a4a221/">
+                        <i className="fa-brands fa-linkedin">
+                            <img src="/linkedin.svg" />
+                        </i>
+                    </a>
+                </span>
             </div>
             <div className={s.photo}></div>
         </div>
