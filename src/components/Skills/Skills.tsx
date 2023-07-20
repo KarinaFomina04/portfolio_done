6e1a28d2b1502a0261751c26babe8f89841c391b
KarinaFomina04 <karinafomina.19970804@gmail.com>
2023-07-20 16:21:13 +0200

    refactor skills

--- src/components/Skills/Skills.tsx
+++ src/components/Skills/Skills.tsx
@@ -1,15 +1,32 @@
 import React from "react";
 import s from './Skills.module.scss';
 import sContainer from '../Common/Styles/Container.module.scss';
+import html5 from './icons/html5.svg';
+import css3 from './icons/css3.svg';
+import js from './icons/js.svg';
+import react from './icons/react.svg';
+import scss from './icons/scss.svg';
 
 const Skills: React.FC = () => {
     return (
         <div className={s.skills}>
             <div className={sContainer.containerb  }>
                 <h2>Tech Stack</h2>
-                <ul>
+                <ul className={s.complex_list}>
                     <li>
-                        <img src="" alt=""/>
+                        <img src={html5} alt="HTML5" width={40}/>
+                    </li>
+                    <li>
+                        <img src={css3} alt="CSS3" width={40}/>
+                    </li>
+                    <li>
+                        <img src={js} alt="JS" width={40}/>
+                    </li>
+                    <li>
+                        <img src={react} alt="REACT" width={40}/>
+                    </li>
+                    <li>
+                        <img src={scss} alt="SCSS" width={40}/>
                     </li>
                 </ul>
             </div>
